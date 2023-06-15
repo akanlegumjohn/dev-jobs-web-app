ad1b89825a53e61ad8c0e80fb799302651f550b4
akanlegumjohn <akanlegumjohn2@gmail.com>
2023-06-15 05:16:32 +0000

    add a functionality to handle user input in order to filter jobs based on inputs

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,12 +1,10 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Navbar from "./components/Navbar";
 import { Home } from "./pages/Home";
 import JobDetails from "./pages/JobDetails";
 
 function App() {
   return (
     <BrowserRouter>
-      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/job/:id" element={<JobDetails />} />
