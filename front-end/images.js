5918962dd7e86577a09bd936f3f6866333ca21d0
akanlegumjohn <akanlegumjohn2@gmail.com>
2023-06-17 04:33:18 +0000

    fix a bug on broken logo images

---
+++ front-end/images.js
@@ -0,0 +1,27 @@
+import scoot from "./src/assets/logos/scoot.svg";
+import blogr from "./src/assets/logos/blogr.svg";
+import vector from "./src/assets/logos/vector.svg";
+import officelite from "./src/assets/logos/officelite.svg";
+import pod from "./src/assets/logos/pod.svg";
+import creative from "./src/assets/logos/creative.svg";
+import pomodoro from "./src/assets/logos/pomodoro.svg";
+import maker from "./src/assets/logos/maker.svg";
+import coffeeroasters from "./src/assets/logos/coffeeroasters.svg";
+import mastercraft from "./src/assets/logos/mastercraft.svg";
+import crowdfund from "./src/assets/logos/crowdfund.svg";
+import typemaster from "./src/assets/logos/typemaster.svg";
+
+export const imageData = [
+  scoot,
+  blogr,
+  vector,
+  officelite,
+  pod,
+  creative,
+  pomodoro,
+  maker,
+  coffeeroasters,
+  mastercraft,
+  crowdfund,
+  typemaster,
+];
